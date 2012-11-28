73cb48648af003b18fb340f6f706518bed7ef3c5
Scott Johnson <7.scott.j@gmail.com>
2012-11-27 20:01:57 -0600

    add initial working rails app

---
+++ Workshop_1/quick_poll/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickPoll::Application.config.secret_token = '1c9b5a065b623572430686f1f9edae5eae64482991188e7ebdfb9dc81f190857b3b8e2a72e0c91a015ca05280a8dcaf66b8d8930b7d9f90726ef7dac6874b394'
