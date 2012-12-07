6aaa5c93a266a0765ab16a46779627ce27226c98
Scott Johnson <7.scott.j@gmail.com>
2012-12-07 00:49:33 -0600

    update choice to match responses

--- Rails101/quick_poll/app/models/choice.rb
+++ Rails101/quick_poll/app/models/choice.rb
@@ -1,5 +1,5 @@
 class Choice < ActiveRecord::Base
-  attr_accessible :count, :position, :question_id, :text
+  attr_accessible :position, :question_id, :text
   belongs_to :question
   
   def display_count
